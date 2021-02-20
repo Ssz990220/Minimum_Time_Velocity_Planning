89ada96c8a386fe6baafe20b9f331ba4d7f79773
Ssz990220 <Ssz990220@sjtu.edu.cn>
2021-02-21 00:04:50 +0800

    Add visualization

--- visualize_result.m
+++ visualize_result.m
@@ -3,13 +3,41 @@ function visualize_result(n_dof, t, tau, qs, qd_t, qdd_t, s, phi, alpha, mu, b)
 %   Detailed explanation goes here
 figure
 plot(s, b)
-title('s-s_dot')
+title('s-sdot')
+
+figure
+title('Torque of joints');
+hold on
+legends = strings(n_dof*2,1);
+for i = 1:n_dof
+    plot(t, tau(:,i));
+    plot(t, repmat(mu(i),size(t,1),1));
+    legends((i-1)*2+1) = sprintf('joint %d',i);
+    legends((i-1)*2+2) = sprintf('torque limit %d',i);
+end
+legend(legends);
+
 
 figure
 hold on
-legends = []
+legends = strings(n_dof*2,1);
+title('Speed of joints');
 for i = 1:n_dof
-    plot(t, tau(:,i));
-    legends = [legends, sprintf('joint %d',i)];
+    plot(t, qd_t(:,i));
+    plot(t, repmat(phi(i),size(t,1),1));
+    legends((i-1)*2+1) = sprintf('joint %d',i);
+    legends((i-1)*2+2) = sprintf('V limit %d',i);
+end
+legend(legends);
+
+figure
+hold on
+legends = strings(n_dof*2,1);
+title('Acceleration of joints');
+for i = 1:n_dof
+    plot(t, qdd_t(:,i));
+    plot(t, repmat(alpha(i),size(t,1),1));
+    legends((i-1)*2+1) = sprintf('joint %d',i);
+    legends((i-1)*2+2) = sprintf('Acc limit %d',i);
 end
 legend(legends);
